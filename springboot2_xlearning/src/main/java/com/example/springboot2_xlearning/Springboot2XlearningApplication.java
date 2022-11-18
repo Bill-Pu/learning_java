cb4853fe8b70c10c444e4d278533173924743800
PYB <puyb@tibetjc.com.cn>
2022-11-18 18:22:17 +0800

    release update

---
+++ springboot2_xlearning/src/main/java/com/example/springboot2_xlearning/Springboot2XlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot2_xlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2XlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2XlearningApplication.class, args);
+	}
+
+}
---
+++ springboot2_xlearning/src/test/java/com/example/springboot2_xlearning/Springboot2XlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot2_xlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2XlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
